ee184bc932917242a5823eed85b6265d28a1a551
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-13 16:04:51 +0530

    initialised authSlice in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import modeReducer from "../slices/modeSlice"
+import authReducer from "../slices/authSlice"
 
 export default configureStore({
     reducer: {
-        mode: modeReducer
+        mode: modeReducer,
+        auth:authReducer
     }
 })
