9e6596a8ae4c99b3389e10f6fcda0776249f7ccb
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-14 15:24:46 +0530

    added a check for expired token in middleware

--- backend/main_server/middlewares/authMiddleware.js
+++ backend/main_server/middlewares/authMiddleware.js
@@ -3,18 +3,23 @@ const User = require("../main_models/user");
 
 function authLogin(req, res, next) {
     const authHeader = req.headers['authorization'];
-    
+
     const accesstoken = authHeader && authHeader.split(" ")[1];
-    
+
     if (!accesstoken)
         return res.status(400).json({ message: "Access token is tampered" });
-    
+
     jwt.verify(accesstoken, process.env.ACCESS_TOKEN_SECRET, async (err, data) => {
-        if (err) return res.status(403).json({ message: "Unauthorized to access" });
+        if (err) {
+            if (err.name === "TokenExpiredError")
+                return res.status(401).json({ message: "Token expired" });
+            
+            return res.status(403).json({ message: "Unauthorized to access" });
+        }
 
         const userData = await User.findById(data.user._id).select("-__v")
         req.user = userData;
-        next(); 
+        next();
     })
 }
 
